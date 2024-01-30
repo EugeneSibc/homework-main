470bda4747399f67d0e34f940ac40c380b82477f
eugenkin <evgensbk9@gmail.com>
2024-01-30 20:17:19 +0700

    refactoring and error correction

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {        
-        onChangeChecked ? onChangeChecked(e.currentTarget.checked) : onChange
+        if(onChangeChecked) {
+            onChangeChecked(e.currentTarget.checked)
+        } else if(onChange){ onChange(e) }
 
     }
 
