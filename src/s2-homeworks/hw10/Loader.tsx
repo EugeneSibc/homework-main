0a4f9ad3c59989c989a1ed304442b3ae54a9f652
eugenkin <evgensbk9@gmail.com>
2024-05-14 21:30:20 +0700

    HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,8 @@
 import s from './Loader.module.css'
+import spiner from './Spinner@1x-1.0s-200px-200px.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <div className={s.loader}>
+         <img src={spiner} />
+    </div>
+)
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,22 @@
+
 const initState = {
     isLoading: false,
 }
-
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type LoadingType = typeof initState
+export const loadingReducer = (state = initState, action: LoadingActionType):LoadingType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {isLoading: action.isLoading}
+        }
         default:
             return state
     }
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+type LoadingActionType = ReturnType<typeof loadingAC>
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
+export const loadingAC = (isLoading: boolean) => ({
     type: 'CHANGE_LOADING',
     isLoading,
 })
