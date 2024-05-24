e13da5fb1cb17c76c75c89c17438fe9ac8131c43
eugenkin <evgensbk9@gmail.com>
2024-05-24 21:08:25 +0700

    HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -13,10 +13,15 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0, 100]))
+    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
     const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-        
+        if(Array.isArray(value)){
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+        }
     }
 
     return (
@@ -39,8 +44,7 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-                            
-                            value={value2}
+                            value={[value1, value2]}
                             onChange={change}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
